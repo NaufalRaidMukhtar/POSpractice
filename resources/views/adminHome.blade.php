112b70830b0852c14fabb45d386ddd0e1f8c0d49
NaufalRaidMukhtar <raidmukhtar2305@gmail.com>
2023-01-30 10:14:18 +0700

    Projek AAS

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Dashboard
+                <a href="{{ route('transaksis.index') }}"> transaksi</a></div>
+                
                 <div class="card-body">
                     You are Admin.
                 </div>
